447da7f90dee77c0d8e12b26f37381cad3bb909d
dellia16 <robert.osvath@barandisolutions.com>
2015-07-07 16:47:52 +0300

    calin work

--- js/main.js
+++ js/main.js
@@ -8,13 +8,16 @@ $(function() {
 		lastName: "Prejmerean",
 		city: "Laponia"
 	}, {
-		firstName: "Tudor",
+		firstName: "Alex",
 		lastName: "Prejmerean",
 		city: "Laponia"
 	}];
 
 	function renderTable() {
-		var $template = $("<tr><td></td><td></td><td></td><td class='action'>X</td></tr>");
+		var $template = $("<tr><td class='firstName'></td>" +
+							"<td class='lastName'></td>" +
+							"<td class='cityName'></td>"+
+							"<td class='action'>X</td></tr>");
 		var $body = $(".user-list tbody");
 
 		for (var i = 0; i < userList.length; i++) {
@@ -52,6 +55,7 @@ $(function() {
 	}
 
 	function addEvents() {
+		var addMode = true;
 		$(".user-list").on("click", ".action", function() {
 			var index = $(this).parent().index();
 			userList.splice(index, 1);
@@ -60,22 +64,74 @@ $(function() {
 			renderTable();
 		});
 
+		$(".user-list").on("click", ".firstName", function(){
+			addMode = false;
+			console.log(addMode);
+			var index = $(this).parent().index();
+			showForm();
+			$(".user-form input[name='firstName']").val(userList[index].firstName);
+			$(".user-form input[name='lastName']").val(userList[index].lastName);
+			$(".user-form input[name='cityName']").val(userList[index].city);
+			$("#saveUser").on("click", function() {
+				userList[index].firstName= $(".user-form input[name='firstName']").val();
+				userList[index].lastName= $(".user-form input[name='lastName']").val();
+				userList[index].city= $(".user-form input[name='cityName']").val();
+				clearTable();
+				renderTable();
+				addMode = true;
+			});
+		});
+ 	
+		$(".user-list").on("click", ".lastName", function(){
+			addMode = false;
+			var index = $(this).parent().index();
+			console.log(userList[index].lastName);
+			showForm();
+			$("#saveUser").on("click", function() {
+					userList[index].firstName= $(".user-form input[name='firstName']").val();
+					userList[index].lastName= $(".user-form input[name='lastName']").val();
+					userList[index].city= $(".user-form input[name='cityName']").val();
+					clearTable();
+				renderTable();
+				addMode = true;
+				});
+		});
+
+			$(".user-list").on("click", ".cityName", function(){
+				addMode = false;
+				var index = $(this).parent().index();
+				console.log(userList[index].city);
+				showForm();
+				$("#saveUser").on("click", function() {
+					userList[index].firstName= $(".user-form input[name='firstName']").val();
+					userList[index].lastName= $(".user-form input[name='lastName']").val();
+					userList[index].city= $(".user-form input[name='cityName']").val();
+					clearTable();
+				renderTable();
+				addMode = true;
+				});
+			});
+
 		$("#addUser").on("click", function() {
 			showForm();
 		});
 
 		$("#saveUser").on("click", function() {
-			var $items = $(".user-form input[type='text']");
-			var obj = {
-				firstName: $($items[0]).val(),
-				lastName: $($items[1]).val(),
-				city: $($items[2]).val()
-			};
+			console.log(addMode);
+			if (addMode == true) {
+			    console.log(addMode);
+				var $items = $(".user-form input[type='text']");
+				var obj = {
+					firstName: $($items[0]).val(),
+					lastName: $($items[1]).val(),
+					city: $($items[2]).val()
+					}
 
 			addUser(obj);
 			hideForm();
 			clearForm();
-		});
+		};
+	});
 
 		$("#cancelUser").on("click", function() {
 			hideForm();

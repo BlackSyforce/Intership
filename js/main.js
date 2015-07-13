5c730da8fbc8a84e3dd24b5646b0cb823450df39
ciogmi <noemi.kun@barandisolutions.com>
2015-07-13 17:41:19 +0300

    tabel cu ajax

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
 $(function() {
-	var userList = [{
-		firstName: "Ion",
-		lastName: "Gheorghe",
-		city: "Iasi"
-	}, {
-		firstName: "Tudor",
-		lastName: "Prejmerean",
-		city: "Laponia"
-	}, {
-		firstName: "Tudor",
-		lastName: "Prejmerean",
-		city: "Laponia"
-	}];
+	var userList = [];
 
 	function renderTable() {
-		var $template = $("<tr><td class='firstName'></td><td class='lastName'></td><td class='city'></td><td class='action'>X</td></tr>");
+		var $template = $("<tr><td class='click'></td><td class='click'></td><td class='click'></td><td class='action'>X</td></tr>");
 		var $body = $(".user-list tbody");
 
 		for (var i = 0; i < userList.length; i++) {
@@ -42,7 +30,7 @@ $(function() {
 		clearTable();
 		renderTable();
 	}
-
+   
 
 	function showForm() {
 		$(".user-form").removeClass("hidden");
@@ -53,49 +41,234 @@ $(function() {
 	}
 
 	function addEvents() {
+		var editMode = true;
+		var editIndex;
+		var Ordonaref = true;
+		var Ordonarel = true;
+		var Ordonarec= true;
+
 		$(".user-list").on("click", ".action", function() {
 			var index = $(this).parent().index();
-			userList.splice(index, 1);
 			
+			$.ajax({
+				"method" : "DELETE",
+				"url" : "http://localhost:4000/user/" + userList[index]._id
+			}).done(function(data) {
+			 	userList.splice(index, 1);
+					clearTable();
+					renderTable();
+
+			});
 			clearTable();
 			renderTable();
 		});
 
+		$(".user-list").on("click", ".click", function() {
+			editMode = true;
+
+            var index = $(this).parent().index();
+            editIndex = index;
+            var user = userList[index];
+            var $items = $(".user-form input[type='text']");
+				var obj = {
+					firstName: $($items[0]).val(),
+					lastName: $($items[1]).val(),
+					city: $($items[2]).val()
+				};
+            showForm();
+            $.ajax({
+				"method" : "PUT",
+				"url" : "http://localhost:4000/user/" + userList[index]._id
+				data: obj
+			}).done(function(data) {
+			 	userList.splice(index, 1);
+			 	
+					clearTable();
+					renderTable();
+
+			});
+        });
+
+		$("#saveUser").on("click", function() {
+			if(editMode){
+
+
+				
+	            var user = userList[editIndex];
+	            var $items = $(".user-form input[type='text']");
+	            user.firstName = $($items[0]).val();
+	            user.lastName = $($items[1]).val();
+	            user.city = $($items[2]).val();
+
+	            clearTable();
+	            renderTable();
+
+	            hideForm();
+	            clearForm();
+			}
+            	
+
+            });
+
 		$("#addUser").on("click", function() {
+			editMode = false;
 			showForm();
+
 		});
 
 		$("#saveUser").on("click", function() {
-			var $items = $(".user-form input[type='text']");
-			var obj = {
-				firstName: $($items[0]).val(),
-				lastName: $($items[1]).val(),
-				city: $($items[2]).val()
-			};
+			if(!editMode){
+		var $items = $(".user-form input[type='text']");
+				var obj = {
+					firstName: $($items[0]).val(),
+					lastName: $($items[1]).val(),
+					city: $($items[2]).val()
+				};
+			$.ajax({
+				"method" : "POST",
+				"url" : "http://localhost:4000/user",
+				data: obj
+			}).done(function(data) {	
+				userList.push(obj);
+				console.log(userList);
+				clearTable();
+				renderTable();
 
-			addUser(obj);
-			hideForm();
-			clearForm();
+			});
+
+
+
+				
+				hideForm();
+				clearForm();
+			}
 		});
+
+		$(".user-list").on("click","#firstName", function() {
+			
+			if(Ordonaref == false){
+				console.log("ordonare descr");
+				for(var i=0;i<userList.length-1;i++) {
+					for(var j=i+1;j<userList.length;j++) {
+						if(userList[i].firstName < userList[j].firstName) {
+							var aux= userList[i].firstName;
+							userList[i].firstName = userList[j].firstName;
+							userList[j].firstName = aux;
+					    }
+					}
+					
+				}
+				Ordonaref = true;
+			} else {
+				console.log("ordonare cresc");
+				for(var i=0;i<userList.length-1;i++) {
+					for(var j=i+1;j<userList.length;j++) {
+						if(userList[i].firstName > userList[j].firstName) {
+							var aux= userList[i].firstName;
+							userList[i].firstName = userList[j].firstName;
+							userList[j].firstName = aux;
+					    }
+					}
+				}
+				Ordonaref= false;
+			}
+
+			clearTable();
+	        renderTable();
+		});
+
+		$(".user-list").on("click","#lastName", function() {
+
+			if(Ordonarel == false){
+				console.log("ordonare descr");
+				for(var i=0;i<userList.length-1;i++) {
+					for(var j=i+1;j<userList.length;j++) {
+						if(userList[i].lastName < userList[j].lastName) {
+							var aux= userList[i].lastName;
+							userList[i].lastName = userList[j].lastName;
+							userList[j].lastName = aux;
+					    }
+					}
+					
+				}
+				Ordonarel = true;
+			} else {
+				console.log("ordonare cresc");
+				for(var i=0;i<userList.length-1;i++) {
+					for(var j=i+1;j<userList.length;j++) {
+						if(userList[i].lastName > userList[j].lastName) {
+							var aux= userList[i].lastName;
+							userList[i].lastName = userList[j].lastName;
+							userList[j].lastName = aux;
+					    }
+					}
+				}
+				Ordonarel = false;
+			}
+
+			clearTable();
+	        renderTable();
+		});
+
+		$(".user-list").on("click","#city", function() {
+			if(Ordonarec == false){
+				console.log("ordonare descr");
+				for(var i=0;i<userList.length-1;i++) {
+					for(var j=i+1;j<userList.length;j++) {
+						if(userList[i].city < userList[j].city) {
+							var aux= userList[i].city;
+							userList[i].city = userList[j].city;
+							userList[j].city = aux;
+					    }
+					}
+					
+				}
+				Ordonarec = true;
+			} else {
+				console.log("ordonare cresc");
+				for(var i=0;i<userList.length-1;i++) {
+					for(var j=i+1;j < userList.length;j++) {
+						if(userList[i].city > userList[j].city) {
+							var aux= userList[i].city;
+							userList[i].city = userList[j].city;
+							userList[j].city = aux;
+					    }
+					}
+				}
+				Ordonarec = false;
+			}
+
+			clearTable();
+	        renderTable();
+		});
+
+		
+			$.ajax({
+				"method" : "GET",
+				"url" : "http://localhost:4000/users"
+			}).done(function(data) {
+			 	for(var i=0;i<data.length; i++){
+					userList[i] = data[i];
+				}
+					clearTable();
+					renderTable();
+
+			});
+
+		
+
+			
+
+			
+		
 
 		$("#cancelUser").on("click", function() {
 			hideForm();
 			clearForm();
 		});
 
-		$(".user-list tr").on("click", ".firstName", function() {
-            var index = $(this).parent().index();
-            var user = userList[index];
-            var $items = $(".user-form input[type='text']");
-            $($items[0]).val(user.firstName);
-            $($items[1]).val(user.lastName);
-            $($items[2]).val(user.city);
-            showForm();
-		});
-
-		
-
 	}
+
 
 
 	renderTable();
